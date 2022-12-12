9737c7c4e8661b437e3852825b122aecf2507746
Ubinquitous <ubinquitous1@gmail.com>
2022-12-13 00:15:16 +0900

    ADD : 헤더 스타일링

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Header.scss';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='header-wrap'>
+            <div className='title'>
+                <Link to='/' className='title-text'>minecraft</Link>
+            </div>
+            <div className='user-in'>
+                <span>3 users in server</span>
+            </div>
         </div>
     );
 };

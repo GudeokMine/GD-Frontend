b66bd381dc31e0d0d8092fafcde79fe4c5c4898a
Ubinquitous <ubinquitous1@gmail.com>
2022-12-13 16:22:46 +0900

    UPDATE : 동접자 수 실시간 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Header.scss';
 
 const Header = () => {
+    const [onlineUser, setOnlineUser] = useState(0);
+
+    const countOnlineUser = () => {
+        axios.get('https://gdmine.kro.kr:1211/mcServer')
+            .then((res) => {
+                if (onlineUser !== res.data.msg) {
+                    setOnlineUser(res.data.msg)
+                }
+                return;
+            })
+            .catch((err) => {
+                return;
+            })
+        return;
+    }
+
+    useEffect(() => {
+        countOnlineUser();
+        setInterval(countOnlineUser, 10000);
+    }, []);
+
     return (
         <div className='header-wrap'>
             <div className='title'>
                 <Link to='/' className='title-text'>minecraft</Link>
             </div>
             <div className='user-in'>
-                <span>3 users in server</span>
+                <span>{onlineUser} users in server</span>
             </div>
         </div>
     );
